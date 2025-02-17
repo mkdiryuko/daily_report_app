2b544a945ab259d39c21892474ae3a786e38e3e8
zmt-yuto <kobayashi.yuto@z-mtech.co.jp>
2025-02-17 13:48:02 +0900

    工数と休みをカレンダーの日付セルを表示する機能の追加

--- routes/calendar.js
+++ routes/calendar.js
@@ -21,7 +21,7 @@ console.log(`今日の年月 : ${current_year}年 ${current_month}月`)
 
 // 総工数/月を返す関数
 async function getMonthTotalPersonHour(year, month) {
-  console.log("---総工数/月の計算---");
+  console.log("---総工数/月の計算---(getMonthTotalPersonHour)");
   try {
     const targetMonth = `${year}-${String(month).padStart(2, '0')}`;
     const result = await knex('daily_report')
@@ -29,14 +29,16 @@ async function getMonthTotalPersonHour(year, month) {
       knex.raw('SEC_TO_TIME(SUM(TIME_TO_SEC(person_hour))) AS total_person_hour')
     )
     .whereRaw("DATE_FORMAT(job_date, '%Y-%m') = ?", [targetMonth]) // 年月でフィルタリング
-    console.log(`${year}年${month}月`);
-    console.log("総工数/月：", result[0]?.total_person_hour);
-    if (result[0].total_person_hour === null) {
+    const total_person_hour = result[0]?.total_person_hour;
+    
+    if (!total_person_hour) {
+      console.log("指定した年月に工数が登録されていませんでした");
       return "00:00";
     }
-    return result[0].total_person_hour.slice(0, -3);
+    return total_person_hour.slice(0, -3); // 秒を削除し、 "hh:mm" フォーマットに
   } catch (error) {
     console.error('Error fetching total person hour:', error);
+    return "00:00";
   } 
 }
 
@@ -46,17 +48,36 @@ async function getTotalPersonHourPerDay(year, month) {
     const targetMonth = `${year}-${String(month).padStart(2, '0')}`;
     const results = await knex('daily_report')
     .whereRaw("DATE_FORMAT(job_date, '%Y-%m') = ?", [targetMonth]) // 年月でフィルタリング
-    .groupByRaw("job_date") // 各日付でグループ化
+    .groupBy("job_date") // 各日付でグループ化
     .select(
-      knex.raw("DATE_FORMAT(job_date, '%Y-%m-%d') AS start"),
-      knex.raw('SEC_TO_TIME(SUM(TIME_TO_SEC(person_hour))) AS title') // 総工数の計算
+      knex.raw("DATE_FORMAT(job_date, '%Y-%m-%d') AS job_date"),
+      knex.raw('SEC_TO_TIME(SUM(TIME_TO_SEC(person_hour))) AS total_person_hour') // 総工数の計算
     )
     .orderBy('job_date', 'asc') // 日付順にソート
     console.log('---総工数/日(JSON)---');
-    console.log(JSON.stringify(results, null, 2));
-    return JSON.stringify(results, null, 2);
+    console.log(results)
+    return results;
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error in getTotalPersonHOurPerDay:', error);
+    return null;
+  }
+}
+
+// 休みの日を返す関数
+async function getAbsenceDays(year, month) {
+  try {
+    const targetMonth = `${year}-${String(month).padStart(2, '0')}`;
+    const results = await knex('absence')
+    .whereRaw("DATE_FORMAT(`date`, '%Y-%m') = ?", [targetMonth])
+    .select(
+      knex.raw("DATE_FORMAT(date, '%Y-%m-%d') AS date")
+    )
+    .orderBy('date', 'asc');
+    console.log("フィルタ結果：", results);
+    return results;
+  } catch (error) {
+    console.error('Error in getAbsenceDays:', error);
+    return null;
   }
 }
 
@@ -76,17 +97,24 @@ router.get('/', async (req, res) => {
 router.get('/api/total_person_hour_pm', async (req, res) => {
   console.log("---総工数/月APIリクエスト---");
   const { year, month } = req.query;
-
-  if (!year || !month) {
-    return res.status(400).json({ error: 'Year and month are required'});
+  console.log("年：", year, "月：", month);
+  
+  // yearやmonthが数値以外またはmonthが無効な月だったらエラーを出す
+  if (!year || !month || isNaN(year) || isNaN(month) || month < 1 || month > 12) {
+    return res.status(400).json({ error: 'Invalid year or month' });
   }
 
   try {
+    console.log("総工数/月の計算開始!!");
     const total_person_hour_pm = await getMonthTotalPersonHour(year, month);
-    console.log(total_person_hour_pd);
+    console.log("取得した総工数/月：", total_person_hour_pm);
+
+    if (!total_person_hour_pm) {
+      return res.status(200).json({ total_person_hour_pm: '00:00' });
+    }
     res.json({ total_person_hour_pm });
   } catch(error) {
-    console.error('総工数/月の取得に失敗しました');
+    console.error('総工数/月の取得に失敗しました(API Error):', error);
     res.status(500).json({ error: 'サーバーエラー'});
   }
 })
@@ -94,19 +122,50 @@ router.get('/api/total_person_hour_pm', async (req, res) => {
 router.get('/api/total_person_hour_pd', async (req, res) => {
   console.log("---総工数/日APIリクエスト");
   const { year, month } = req.query;
-  console.log('year', year);
-  console.log('month', month);
+  console.log(year,'年', month, '月');
 
-  if(!year || !month ) {
-    return res.status(400).json({ error: 'year and month are required' });
+  // yearやmonthが数値以外またはmonthが無効な月だったらエラーを出す
+  if (!year || !month || isNaN(year) || isNaN(month) || month < 1 || month > 12) {
+    return res.status(400).json({ error: 'Invalid year or month' });
   }
 
   try {
+    console.log("総工数/日の計算開始!!")
     const total_person_hour_pd = await getTotalPersonHourPerDay(year, month);
-    res.json( {total_person_hour_pd} );
+
+    // データ取得失敗時の処理
+    if (!total_person_hour_pd) {
+      return res.status(500).json({ error: 'Failed to fetch total person hours' });
+    }
+    // 結果をJSONとして返す
+    res.json( total_person_hour_pd );
   } catch(error) {
     console.error('総工数/日の取得に失敗しました');
     res.status(500).json({ error: 'サーバーエラー'});
+  }
+})
+
+router.get('/api/absence', async (req, res) => {
+  console.log('---休みの日取得APIリクエスト---');
+  const { year, month } = req.query;
+
+  // yearやmonthが数値以外またはmonthが無効な月だったらエラーを出す
+  if (!year || !month || isNaN(year) || isNaN(month) || month < 1 || month > 12) {
+    return res.status(400).json({ error: 'Invalid year or month' });
+  }
+
+  try {
+    console.log('---休みの日を取得---');
+    const absence_day = await getAbsenceDays(year, month);
+
+    if (!absence_day) {
+      return res.status(500).json({ error: 'Failed to fetch absence day' });
+    }
+
+    res.json( absence_day );
+  } catch (error) {
+    console.error('休みの日の取得に失敗しました');
+    res.status(500).json({ json: 'サーバーエラー'});
   }
 })
 
